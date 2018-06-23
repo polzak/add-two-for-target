4ede4bf6194aef2efdbc6fac84ac28afd6d7a624
polzak <hello.polzak@gmail.com>
2018-06-23 17:27:18 +0900

    added comments and test functions

--- index.js
+++ index.js
@@ -1,28 +1,73 @@
 const addTwoForTarget = (arr) => {
+
+    //check if the given array meets the condition
     if (arr.length !== 2 || typeof(arr[0]) !== 'object' || typeof(arr[1]) !== 'number') {
         return `Sorry, the format of the given array is not valid.`;
     }
 
+    //we can't add if there is no more than one number
     if (arr[0].length < 2) {
         return `Sorry, there should be at least TWO numbers in the given array.`;
     }
 
-    const givenArr = arr[0];
-    const target = arr[1];
-    const answer = [];
+    //filter the array
+    const givenArr = arr[0].filter(el => typeof(el) === 'number');
 
-    while (givenArr.length > 1) {
-        let firstNum = givenArr.pop();
-        let idx = givenArr.indexOf(target - firstNum);
-        if (idx > -1) {
-            answer.push(givenArr[idx]);
-            answer.push(firstNum);
+    //check the array's length again after filtering
+    if (givenArr.length < 2) {
+        return `Sorry, there should be at least TWO numbers in the given array.`;
+    }
+
+    const target = arr[1]; //the target number
+    const answer = []; //if a match of two numbers are found, we push them into this array and return it
+
+    while (givenArr.length > 1) { 
+        let firstNum = givenArr.pop(); //each number is removed from the array and checked by turn through the loop
+        let idx = givenArr.indexOf(target - firstNum); //we are looking for a match that could be added by firstNum to produce the target number
+        if (idx > -1) {                 //if we find one
+            answer.push(givenArr[idx]); //we push the match number into the answer array
+            answer.push(firstNum);      //and the firstNum
             return answer;
         }
     }
+
     return `There is no match of two numbers that are added to produce the target number.`;
 }
 
-const arr1 = [[1, 3, 5, 7, 9], 12];
-const result = addTwoForTarget(arr1);
-console.log(result);
+//const arr1 = [[1, '3', '5', '7', '9'], 12];
+// const result = addTwoForTarget(arr1);
+// console.log(result);
+
+//a test function that generates a random array and a random target
+const randomTest = () => {
+    const generateRandomTestArray = () => {
+
+        const NUMBERS_RANGE = 20; //the range of numbers for a given array
+        const NUMBER_OF_NUMBERS = 10; //the number of numbers for a given array
+        const TARGET_RANGE = 30; //the range of a target number
+
+        const generateRandomArray = () => {
+
+            const arr = [];
+            let el;
+            for (let i=0; i<NUMBER_OF_NUMBERS; i++) {
+                el = Math.floor(Math.random()*NUMBERS_RANGE) + 1;
+                arr.push(el);
+            }
+            return arr;
+        }
+    
+        const generateRandomTarget = () => {
+            return Math.floor(Math.random()*TARGET_RANGE) + 1;
+        }    
+    
+        return [generateRandomArray(), generateRandomTarget()];
+    }
+    
+    const randomTestArray = generateRandomTestArray();
+    const randomResult = addTwoForTarget(randomTestArray);
+    console.log('Given Array:', randomTestArray);
+    console.log('Result:', randomResult);
+}
+
+randomTest();
---
+++ readme.md
@@ -0,0 +1,18 @@
+# add-two-for-target Function
+## This function is submitted for [Zero-to-mastery Coding Challenge 6](https://github.com/zero-to-mastery/Coding_Challenge-6)
+
+Given an array of numbers and a target number, this function tries to find a match of two numbers in the array that can be added to produce the target number. If it succeeds, it returns the match of two numbers in an array. Otherwise, it returns "There is no match of two numbers that are added to produce the target number." 
+
+### Example
+#### given [ [ 9, 4, 5, 4, 1 ], 10 ]
+Result: [ 9, 1 ]
+
+#### given [ [ 2, 3, 3, 5, 6 ], 4 ]
+Result: There is no match of two numbers that are added to produce the target number.
+
+### Test
+The index.js file includes a test function that generates a random array and a random target number. You can set some constants to vary the numbers and range of the numbers.
+
+> const NUMBERS_RANGE: set the range of numbers for a given array
+> const NUMBER_OF_NUMBERS: set the number of numbers for a given array
+> const TARGET_RANGE: set the range of a target number
